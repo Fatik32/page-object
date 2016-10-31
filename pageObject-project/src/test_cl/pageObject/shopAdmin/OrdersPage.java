7bef2b1b136c59e9936a4ce5441ca4a810db9c71
ayufatov <Fatik32rus>
2016-10-31 20:03:48 +0300

    add AccountsIdPage and AccountsIdPageTest

---
+++ pageObject-project/src/test_cl/pageObject/shopAdmin/OrdersPage.java
@@ -0,0 +1,7 @@
+package test_cl.pageObject.shopAdmin;
+
+/**
+ * Created by ayufatov on 31.10.2016.
+ */
+public class OrdersPage {
+}
---
+++ pageObject-project/src/test_cl/tests/shopAdmin/OrdersPageTest.java
@@ -0,0 +1,7 @@
+package test_cl.tests.shopAdmin;
+
+/**
+ * Created by ayufatov on 31.10.2016.
+ */
+public class OrdersPageTest {
+}
